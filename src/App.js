04909f58521b360aad5329f5ec6414f9a4af827c
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-12-18 11:17:17 -0600

    [components] Add about and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
-import Welcome from './components/Welcome'
+import Welcome from './components/Welcome';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is about us...</p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, facilis illum assumenda in hic, error quas nihil et libero quibusdam eveniet exercitationem nisi quam eaque officia quae dolore excepturi deleniti?
+            </p>
+        </div>
+    )
+}
+
+export default About;
